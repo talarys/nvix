12b4dc65fcf2e1472fe5ed1147b9978fddb18bfa
talarys <d.simon.ssx0@gmail.com>
2025-08-06 20:03:34 +0200

    add jj to escape

--- plugins/common/mappings.nix
+++ plugins/common/mappings.nix
@@ -28,6 +28,7 @@ let
 
   insert = [
     (mkKeymap "i" "jk" "<esc>" "Normal Mode")
+    (mkKeymap "i" "jj" "<esc>" "Normal Mode")
     (mkKeymap "i" "<c-s>" "<esc>:w ++p<cr>" "Save file")
     (mkKeymap "i" "<a-j>" "<esc>:m .+1<cr>==gi" "Move Line Down")
     (mkKeymap "i" "<a-k>" "<esc>:m .-2<cr>==gi" "Move Line Up")
