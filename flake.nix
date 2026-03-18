e235b827802a4822d11d4af9cc5920b5f97a4b36
talarys <d.simon.ssx0@gmail.com>
2026-03-18 19:15:01 +0000

    Removed cache

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
   description = "Neovim configuation";
 
-  nixConfig = {
-    allow-unfree = true;
-    extra-substituters = "https://nvix.cachix.org";
-    extra-trusted-public-key = "nvix.cachix.org-1:qVYAfj2oiH0DF3pSs8OfPYI6B0mAZ+h5mMajN+EOL2E=";
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
