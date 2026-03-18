cc2564245c6e8041512d5884ecc18299158b6fec
talarys <d.simon.ssx0@gmail.com>
2026-03-18 19:14:24 +0000

    Colorscheme update

--- plugins/common/colorscheme.nix
+++ plugins/common/colorscheme.nix
@@ -1,4 +1,3 @@
-{ config, ... }:
 {
   colorschemes = {
     catppuccin = {
@@ -13,30 +12,15 @@
             information = [ "undercurl" ];
           };
         };
+        float = {
+          transparent = true;
+          solid = false;
+        };
         flavor = "mocha";
         italic = true;
         bold = true;
         dimInactive = false;
         transparent_background = true;
-      };
-    };
-  };
-
-  colorschemes.tokyonight = {
-    enable = false;
-    settings = {
-      style = "night";
-      transparent = config.nvix.transparent;
-      styles = {
-        floats = if config.nvix.transparent then "transparent" else "dark";
-        sidebars = if config.nvix.transparent then "transparent" else "dark";
-        comments.italic = true;
-        functions.italic = true;
-        variables.italic = true;
-        keywords = {
-          italic = true;
-          bold = true;
-        };
       };
     };
   };
