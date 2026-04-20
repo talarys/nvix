075156bd7aac4aee342b6e41a1e26419b031c643
talarys <d.simon.ssx0@gmail.com>
2026-04-20 15:50:19 +0000

    Copilot accept only single line

--- plugins/blink-cmp.nix
+++ plugins/blink-cmp.nix
@@ -16,24 +16,16 @@ in
               # lua
               mkRaw ''
                 function(cmp)
-                  local ok,_ = pcall(require, "copilot")
-                    if ok then
-                      vim.g.copilot_no_tab_map = true
-                      vim.g.copilot_assume_mapped = true
-                      vim.g.copilot_tab_fallback = ""
-
-                      local suggestion = require("copilot.suggestion")
-                      if suggestion.is_visible() then
-                        suggestion.accept()
-                      else
-                        if cmp.snippet_active() then
-                        return cmp.select_and_accept()
-                        else
-                        return cmp.accept()
-                        end
-                      end
-                    end
+                  local ok, copilot = pcall(require, "copilot.suggestion")
+                  if ok and copilot.is_visible() then
+                    copilot.accept_line()
+                    return true
+                  elseif cmp.snippet_active() then
+                    return cmp.select_and_accept()
+                  else
+                    return cmp.accept()
                   end
+                end
               ''
             )
           ];
